7f1a6fc38b2c5b7854a7545217356ccfa7ff15fb
pinarcanpolat <pinarcanpolat@live.com>
2021-11-04 21:25:36 +0300

    navi,productlist,categorylist add

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import Navi from './Navi'
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
 
 function App() {
   return (
     <div>
-      <h2>hello from react</h2>
+      <Navi></Navi>
+      <CategoryList></CategoryList>
+      <ProductList></ProductList>
     </div>
   );
 }
